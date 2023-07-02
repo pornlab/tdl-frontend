bc1a54c070d6368d40d61fdc6645104fc8e93a45
iandreev <iandreev@wowworks.ru>
2023-07-03 00:16:19 +0900

    add questions to themes

--- packages/app/features/dbList/helpers/getTheme.ts
+++ packages/app/features/dbList/helpers/getTheme.ts
@@ -4,7 +4,7 @@ import vehicleLaw from '../themes/vehicleLaw.json'
 import vehicleMaintenance from '../themes/vehicleMaintenance.json'
 import techniquesOfSafeDriving from '../themes/techniquesOfSafeDriving.json'
 
-const shuffle = (array: Question[]) => array.sort(() => Math.random() - 0.5)
+export const sortRandom = (arr: Question[]) => arr.sort(() => Math.random() - 0.5)
 
 export const getAllQuestions = () => {
   return testQuestions
@@ -16,20 +16,16 @@ export const getQuestionsByTheme = (theme: ThemeTypes | string): Question[] => {
   }
   switch (theme) {
     case ThemeTypes.VehicleLaw:
-      return shuffle(vehicleLaw)
+      return sortRandom(vehicleLaw)
     case ThemeTypes.VehicleMaintenance:
-      return shuffle(vehicleMaintenance)
+      return sortRandom(vehicleMaintenance)
     case ThemeTypes.TechniquesOfSafeDriving:
-      return shuffle(techniquesOfSafeDriving)
+      return sortRandom(techniquesOfSafeDriving)
     default:
-      return shuffle(testQuestions)
+      return sortRandom(testQuestions)
   }
   // return testQuestions.filter((question) => question.theme === theme)
 }
 
-export const sortRandom = (arr: Question[]) => arr.sort(() => Math.random() - 0.5)
-
-export const getQuestionsAndSortByTheme = (theme: ThemeTypes | string): Question[] =>
-  getQuestionsByTheme(theme).sort(() => Math.random() - 0.5)
-
-export const getMaraphoneQuestions = () => sortRandom(testQuestions)
+export const getMaraphoneQuestions = () =>
+  sortRandom([...vehicleLaw, ...vehicleMaintenance, ...techniquesOfSafeDriving])
